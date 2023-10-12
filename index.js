36e76beca3a69849f3790a3eeaa6249a5df17387
MohamadSheikhAlshabab <alshabab.moh@gmail.com>
2023-10-12 15:41:19 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const reversedWord = word.split('').reverse().join('');
+  return reversedWord === word;
 }
 
 /* 
   Add your pseudocode here
+  reverse  === word
 */
 
 /*
   Add written explanation of your solution here
+  1- convert string to array using split("") method.
+  2- then reverse all characters using reverse() method.
+  3- then joined all characters after reverse them.
+  4- then check if reversed word equal and the same the word before reversed. 
 */
 
 // You can run `node index.js` to view these console logs
